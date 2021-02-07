6258dbe92e135cf1c9f157f87468f1dcfe17ff7e
princ-od-zmijavaca <kneza@outlook.com>
2021-02-08 00:23:31 +0100

    cleanup 3
    cleanup 3

--- moneys-web/src/pages/Settings.jsx
+++ moneys-web/src/pages/Settings.jsx
@@ -4,7 +4,7 @@ export default function SettingsPage() {
   return (
     <div className="settings-page">
       <h1>Settings Page</h1>
-      <h2>email :</h2>
+      <h2>email</h2>
     </div>
   );
 }
